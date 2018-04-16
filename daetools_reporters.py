0ee2afb72c2b5657c5d0cb5bda18d5e44551a8a5
Jaime Souza <jaimenms@gmail.com>
2018-04-15 22:53:38 -0300

    New test

---
+++ daetools_reporters.py
@@ -0,0 +1,20 @@
+import os
+from time import localtime, strftime
+from daetools.pyDAE.data_reporters import daeDelegateDataReporter, daeJSONFileDataReporter
+
+def setupDataReporters(simulation):
+
+    datareporter = daeDelegateDataReporter()
+
+    dr = daeJSONFileDataReporter()
+
+    datareporter.AddDataReporter(dr)
+
+    json_filename = simulation.report_filename
+    simName = os.path.basename(json_filename) + '_' + strftime(" [%d.%m.%Y %H:%M:%S]", localtime())
+    dr.Connect(json_filename, simName)
+
+    print('%s (%s): %s' % (
+    dr.__class__.__name__, dr.ConnectString, 'connected' if dr.IsConnected() else 'NOT connected'))
+
+    return datareporter
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -9,10 +9,7 @@ sys.path.insert(0,parentdir)
 
 from daetools.pyDAE import *
 
-from daetools_reporters import setupDataReporters
-
-
-class mod_test2(daeModel):
+class mod_test1(daeModel):
 
     def __init__(self, Name, Parent=None, Description=""):
 
@@ -21,12 +18,13 @@ class mod_test2(daeModel):
         daeModel.__init__(self, Name, Parent, Description)
 
 
-        self.Q = daeParameter("Q", W, self, "Power")
+        self.Q = daeParameter("En", J, self, "Heat")
         self.m = daeParameter("m", kg, self, "Mass")
         self.cp = daeParameter("c_p", J / (kg * K), self, "Specific heat capacity")
+        self.T0 = daeParameter("T0", K, self, "Initial temperature")
 
         self.T = daeVariable("T", temperature_t, self)
-        self.T.Description = "Temperature"
+        self.T.Description = "Final Temperature"
 
 
     def DeclareEquations(self):
@@ -34,59 +32,51 @@ class mod_test2(daeModel):
         daeModel.DeclareEquations(self)
 
         eq = self.CreateEquation("HeatBalance", "Heat balance")
-        eq.Residual = dt(self.m() * self.cp() * self.T()) - self.Q()
+        eq.Residual = self.Q() - self.m() * self.cp() * ( self.T() - self.T0())
 
 
-
-class sim_test2(daeSimulation):
+class sim_test1(daeSimulation):
 
     def __init__(self):
 
         daeSimulation.__init__(self)
 
-        self.m = mod_test2("test1")
+        self.m = mod_test1("test1")
         self.m.Description = "Testing the solution of a linear equation"
         self.report_filename = __file__ + '.json'
 
     def SetUpParametersAndDomains(self):
 
-        self.m.Q.SetValue(385 * W)
+        self.m.Q.SetValue(10 * 385 * J)
         self.m.cp.SetValue(385 * J / (kg * K))
         self.m.m.SetValue(1 * kg)
-
-    def SetUpVariables(self):
-        # SetInitialCondition function in the case of distributed variables can accept additional arguments
-        # specifying the indexes in the domains. In this example we loop over the open x and y domains,
-        # thus we start the loop with 1 and end with NumberOfPoints-1 (for both domains)
-        self.m.T.SetInitialCondition(300 * K)
+        self.m.T0.SetValue(300 * K)
 
 
-def test_test2():
+def test_test1():
 
-    sim = sim_test2()
+    from daetools_reporters import setupDataReporters
+
+    sim = sim_test1()
 
     daeActivity.simulate(
         sim,
-        reportingInterval=1,
-        timeHorizon=10,
+        reportingInterval=5,
+        timeHorizon=500,
         datareporter=setupDataReporters(sim),
         guiRun=False)
 
     data = json.load(open(sim.report_filename))
 
-    from scipy.interpolate import interp1d
-    from numpy import asscalar
-    f = interp1d(data['T']['Times'],data['T']['Values'])
-
-    assert round(asscalar(f(10)) - 310.0, 4) == 0.0
+    assert data['T']['Values'][0] == 310.0
 
 
 
 if __name__ == "__main__":
 
-    simulation = sim_test2()
+    simulation = sim_test1()
 
-    daeActivity.simulate(simulation, reportingInterval=1, timeHorizon=10,
+    daeActivity.simulate(simulation, reportingInterval=5, timeHorizon=500,
                          datareporter=setupDataReporters(simulation), guiRun=False)
 
     data = json.load(open(simulation.report_filename))
